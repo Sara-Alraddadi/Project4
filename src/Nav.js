96a7322d153f3d07370ab14ac217203341f66ac6
Sara-Alraddadi <sara.alraddadi5@outlook.com>
2020-02-05 09:45:22 +0300

    added header to home

--- src/Nav.js
+++ src/Nav.js
@@ -1,26 +1,27 @@
 import React, {Component} from 'react';
+import { StickyContainer, Sticky } from 'react-sticky';
 import './nav.css';
 
 class Nav extends Component{
   render () {
     return (
        <div>
-          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a class="navbar-brand" href="#">Photography</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <nav sticky="top" className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">Photography</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
-              <a class="nav-item nav-link" href="/PhotographerProfile">PhotographerProfile</a>
-              <a class="nav-item nav-link" href="/PostAndOffers">PostAndOffers</a>
-              <a class="nav-item nav-link" href="/PostForm">PostForm</a>
-              <a class="nav-item nav-link" href="/LoOfferFormgin">LoOfferFormgin</a>
-              <a class="nav-item nav-link" href="/PostUser">PostUser</a>
-              <a class="nav-item nav-link" href="/OfferPhoto">OfferPhoto</a>
-              <a class="nav-item nav-link" href="/Login">Login</a>
-              <a class="nav-item nav-link" href="/Signup">Signup</a>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-item nav-link active" href="/">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-item nav-link" href="/PhotographerProfile">PhotographerProfile</a>
+              <a className="nav-item nav-link" href="/PostAndOffers">PostAndOffers</a>
+              <a className="nav-item nav-link" href="/PostForm">PostForm</a>
+              <a className="nav-item nav-link" href="/LoOfferFormgin">LoOfferFormgin</a>
+              <a className="nav-item nav-link" href="/PostUser">PostUser</a>
+              <a className="nav-item nav-link" href="/OfferPhoto">OfferPhoto</a>
+              <a className="nav-item nav-link" href="/Login">Login</a>
+              <a className="nav-item nav-link" href="/Signup">Signup</a>
             </div>
             </div>
         </nav>
